f7c244782eb623cb76bd79ee2fc878c4a7b108d1
Keerthana Sriranga <keerthanasriranga11@gmail.com>
2017-08-01 14:29:56 +0530

    Created

---
+++ Cracking The Coding Interview/arrayLeftRotation.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        int temp=0,j;
+        for(int i=0; i < n; i++){
+           int newLocation = (i + (n - k)) % n;
+           a[newLocation] = in.nextInt();
+        }
+           for(int i=0; i < n; i++)System.out.print(a[i]+" ");
+        }
+       
+}
