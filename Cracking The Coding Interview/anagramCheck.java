16681c9808c71727a5ef42a6d9fb908b57a2c418
Keerthana Sriranga <keerthanasriranga11@gmail.com>
2017-08-01 20:13:02 +0530

    Added Problem Statement

--- Cracking The Coding Interview/anagramCheck.java
+++ Cracking The Coding Interview/anagramCheck.java
@@ -1,4 +1,7 @@
-//Given two strings, a and b, that may or may not be of the same length, determine the minimum number of character deletions required to make a and b anagrams. Any characters can be deleted from either of the strings
+//Problem Statement:
+//Given two strings, a and b, that may or may not be of the same length,
+//determine the minimum number of character deletions required to make a and b anagrams. 
+//Any characters can be deleted from either of the strings
 
 import java.io.*;
 import java.util.*;
