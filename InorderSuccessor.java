032f22756e6b1b21d655a4a44ae21f957d7ef4fc
Keerthana Sriranga <keerthanasriranga11@gmail.com>
2017-08-08 10:45:28 +0530

    Update InorderSuccessor.java

--- InorderSuccessor.java
+++ InorderSuccessor.java
@@ -56,14 +56,14 @@ Node inOrderSuccessor(Node root, Node n) {
     /* Given a non-empty binary search tree, return the minimum data 
      value found in that tree. Note that the entire tree does not need
      to be searched. */
-    Node minValue(Node node) {
+   /* Node minValue(Node node) {
         Node current = node;
  
         /* loop down to find the leftmost leaf */
-        while (current.left != null) {
+       /* while (current.left != null) {
             current = current.left;
         }
         return current;
-    }*/
+    } */
  
 
